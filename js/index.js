c70a96a3fc2ceedceb09a39b48896f4496afbc64
eman khaled <91575758+emankhal@users.noreply.github.com>
2024-12-19 01:53:14 +0200

    index.js

--- js/index.js
+++ js/index.js
@@ -1,53 +1,48 @@
+var loginBtn = document.getElementById("loginBtn");
+var login = document.querySelector("#login");
+var currentEmail = document.getElementById("currentEmail");
+var currentPassword = document.getElementById("currentPassword");
+var messAlert = document.getElementById("alert");
+var loginValues = [];
 
-var loginBtn = document.getElementById("loginBtn")
-var login = document.querySelector("#login")
-var currentEmail = document.getElementById("currentEmail")
-var user = document.getElementById("user")
-var currentPassword = document.getElementById("currentPassword")
-var messAlert = document.getElementById("alert")
-var loginValues = []
-
-// -----------------------------------------------------------
 login.addEventListener("submit", function (e) {
     e.preventDefault();
     e.stopPropagation();
     var loginData = {
         currentEmail: currentEmail.value,
-        currentPassword: currentPassword.value,
-    }
-    loginValues.push(loginData)
-    signData = JSON.parse(localStorage.getItem("data"))
+        currentPassword: currentPassword.value
+    };
+    loginValues.push(loginData);
+    
+    var signData = JSON.parse(localStorage.getItem("data"));
     if (signData) {
-        for (let i = 0; i < JSON.parse(localStorage.getItem("data")).length; i++) {
-            if (signData[i].email == currentEmail.value && signData[i].password == currentPassword.value) {
-                window.location.href = "../home.html"
-                messAlert.classList.add("d-none")
-
-                user.innerHTML += `${signData[i].myName}`;
-                targetName=signData[i].myName
-                
-
-
-            }
-            else {
-                messAlert.classList.remove("d-none")
+        let isLoggedIn = false;
+        for (let i = 0; i < signData.length; i++) {
+            if (signData[i].email === currentEmail.value && signData[i].password === currentPassword.value) {
+                isLoggedIn = true;
+                window.location.href = "../home.html";
+                messAlert.classList.add("d-none");
+                localStorage.setItem("loggedName", JSON.stringify(signData[i].myName));
+                break;
             }
         }
-
+        
+        if (!isLoggedIn) {
+            messAlert.innerHTML = "Incorrect email or password. Please try again.";
+            messAlert.classList.remove("d-none");
+        }
+    } else if (currentEmail.value === "" || currentPassword.value === "") {
+        messAlert.innerHTML = "Email or password field is empty.";
+        messAlert.classList.remove("d-none");
+    } else {
+        messAlert.innerHTML = "Please sign up.";
+        messAlert.classList.remove("d-none");
     }
-    else {
-        messAlert.innerHTML = "please sign-up"
-        messAlert.classList.remove("d-none")
-    }
-    clearInput()
-
-})
-console.log(user);
-
+    
+    clearInput();
+});
 
 function clearInput() {
-    usernName.value = "";
-    email.value = ""
-    password.value = ""
+    currentEmail.value = "";
+    currentPassword.value = "";
 }
-
