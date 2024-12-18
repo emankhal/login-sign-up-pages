6b37686d961e885c4ba21390d052af69c9b5b63c
eman khaled <91575758+emankhal@users.noreply.github.com>
2024-12-19 01:53:48 +0200

    home.js

--- js/home.js
+++ js/home.js
@@ -1,8 +1,15 @@
+var user = document.getElementById("user");
+var logoutBtn = document.getElementById("logout");
+var loggedName = JSON.parse(localStorage.getItem("loggedName")); 
 
-var user =document.getElementById("user")
-logoutBtn=document.getElementById("logout")
-user.innerHTML += `${JSON.parse(localStorage.getItem("data"))[0].myName}`;
-logoutBtn.addEventListener("click",function(){
+if (loggedName) {
+    user.innerHTML = loggedName;
+}
+
+logoutBtn.addEventListener("click", function() {
+    
     window.location.href="../index.html"
-})
+    localStorage.removeItem("loggedName"); 
+    user.innerHTML = "";
+});
 
